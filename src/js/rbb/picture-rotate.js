8a60499d3199fe4d45dbb4a2a51b21002f0d0c71
Bucinell, Ryan <rbucinell.dev@gmail.com>
2022-03-11 14:38:30 -0500

    if manifest exists, use that to load images

--- src/js/rbb/picture-rotate.js
+++ src/js/rbb/picture-rotate.js
@@ -1,11 +1,23 @@
-const delay = 5000;
-
 document.addEventListener('DOMContentLoaded', async ()=> {
+	
+	const delay = 5000;
 
 	let images = [];
 	let foundMax = false;
-	let cur = 1;	
+	let cur = 1;
 	let portrait = document.querySelector("#profile > div.portrate");
+
+	//If there is a manifest, then preload the rotation images
+	let manifest = await fetch( 'manifest.json' );
+	if( manifest.status != 404 )
+	{
+		let manifestJSON = await manifest.json();
+		for( let img of manifestJSON.images )
+		{
+			images.push( await fetch( `img/rotation/${img}`).then( response => response.blob() ) );
+		}
+		foundMax = true;
+	}
 
 	while( true )
 	{
@@ -15,81 +27,10 @@ document.addEventListener('DOMContentLoaded', async ()=> {
 			if( imgResponse.status === 404 ) 
 				foundMax = true;
 			else
-				images.push(  await imgResponse.blob() );
+				images.push( await imgResponse.blob() );
 		}
 		portrait.style.backgroundImage =  `url(${URL.createObjectURL(images[(cur-1)%images.length])})`;
 		await new Promise(resolve => setTimeout(resolve, delay));
 		cur++;
 	}
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-//DAD, just update to the new number of the highest PIC##.jpg
-var number_of_pictures = 50;
-var SECONDS = 3;
-var badfiles = [];
-var goodfiles = [];
-var cur = 1;
-var image;
-var $portrait = {};
-
-
-function updatePortrait()
-{
-	var nextSrc = "img/rotation/PIC" + ((cur < 10 ) ? "0" + cur : cur) + ".jpg";
-	
-	if(  cur == number_of_pictures )
-	{
-		cur = 1;
-	}
-	if( badfiles.indexOf(cur) >= 0 )
-	{
-		cur++;
-		updatePortrait();
-	}
-	else
-	{
-		$.ajax({
-		url: nextSrc,
-		type: 'get',
-		success: function(data)
-		{
-			console.log(nextSrc);
-			$portrait.css('background-image', 'url(data:image/jpg;base64,' +data + ')');
-			//$portrait.attr('src', "data:image/png;base64," + data); //todo, figure out why this isn't setting correctly
-		},
-		error: function()
-		{
-			number_of_pictures = cur-1;
-			cur = 0;
-		},
-		complete: function()
-		{
-			cur++;
-			setTimeout(updatePortrait, SECONDS * 1000 );
-		}		
-		});
-	}	
-}
-
-$(function(){	
-	$portrait = $("#profile > div.portrate");
-	$portrait.css("all ease 3s");
-	
-	updatePortrait();
-});
-*/
