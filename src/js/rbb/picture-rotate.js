b2fb038ca1165743e3c6dae301010a754851c740
Ryan Bucinell <rbucinell.dev@gmail.com>
2023-07-31 16:18:05 -0400

    commiting astro work

---
+++ src/js/rbb/picture-rotate.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', async ()=> {
+	
+	const delay = 5000;
+
+	let images = [];
+	let foundMax = false;
+	let cur = 1;
+	let portrait = document.querySelector("#profile > img");
+
+	//If there is a manifest, then preload the rotation images
+	let manifest = await fetch( 'manifest.json' );
+	if( manifest.status != 404 )
+	{
+		let manifestJSON = await manifest.json();
+		for( let img of manifestJSON.images )
+		{
+			images.push( await fetch( `img/rotation/${img}`).then( response => response.blob() ) );
+		}
+		foundMax = true;
+	}
+
+	while( true )
+	{
+		if( !foundMax )
+		{
+			let imgResponse = await fetch( `img/rotation/PIC${((cur < 10 ) ? "0" + cur : cur)}.jpg` );
+			if( imgResponse.status === 404 ) 
+				foundMax = true;
+			else
+				images.push( await imgResponse.blob() );
+		}
+		portrait.style.backgroundImage =  `url(${URL.createObjectURL(images[(cur-1)%images.length])})`;
+		await new Promise(resolve => setTimeout(resolve, delay));
+		cur++;
+	}
+});
